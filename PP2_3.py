c262dd2cb5014cab92eec71a20a76e966d965389
Ezhil-Sekar-ICD201-01 <166552821+Ezhil-Sekar-ICD201-01@users.noreply.github.com>
2024-10-16 13:43:15 +0000

    e

--- PP2_3.py
+++ PP2_3.py
@@ -2,11 +2,23 @@
 
 def q1(): 
   #Write Assignment code here
-
+ word = input("In: ")
+ if word[-3:] == "ife":
+    print("-ives")
+ elif word[-2:] == "ey":
+  print("-eys")
+ elif word[-1:] == "y":
+  print("-ies")
+ else:
+  print("-s")
 
 def q2(): 
   #Write Assignment code here
-
+  num = int(input("In: "))
+  if num%2 == 0:
+    print(f'{num} is positve')
+  elif num%2 != 0:
+    print(f'{num} is negative')
 
 
 #Do not alter the following code
