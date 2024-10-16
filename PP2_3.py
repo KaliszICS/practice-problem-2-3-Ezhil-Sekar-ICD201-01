c1251b923d2958b83cdda04bbc154fcd7099401f
Ezhil-Sekar-ICD201-01 <166552821+Ezhil-Sekar-ICD201-01@users.noreply.github.com>
2024-10-16 13:57:32 +0000

    e

--- PP2_3.py
+++ PP2_3.py
@@ -15,14 +15,35 @@ def q1():
 def q2(): 
   #Write Assignment code here
   num = int(input("In: "))
-  if num%2 == 0:
-    print(f'{num} is positve')
-  elif num%2 != 0:
-    print(f'{num} is negative')
+  if num>0:
+    print(f"{num} is positve")
+  elif num<0:
+    print(f"{num} is negative")
+
+  
+def q3():
+  side1 = float(input("Input a number: "))
+  side2 = float(input("Input a number: "))
+  side3 = float(input("Input a number: "))
+
+  if (side1 + side2 > side3) and (side1 + side3 > side2) and (side2 + side3 > side1):
+    if side1 == side2 == side3:
+        print("Equilateral")
+    elif side1 == side2 or side1 == side3 or side2 == side3:
+      print("Isosceles")
+    else:
+      print("Scalene")
+  else:
+    print("No Triangle")
+
+
+  
 
 
 #Do not alter the following code
 #Comment out the following code when running your tests
-
+'''
 q1()
 q2()
+q3()
+'''
