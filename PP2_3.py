d3c70425a6716e5a8408b4722df921652d1cb0bb
Ezhil-Sekar-ICD201-01 <166552821+Ezhil-Sekar-ICD201-01@users.noreply.github.com>
2024-10-16 13:58:49 +0000

    e

--- PP2_3.py
+++ PP2_3.py
@@ -16,7 +16,7 @@ def q2():
   #Write Assignment code here
   num = int(input("In: "))
   if num>0:
-    print(f"{num} is positve")
+    print(f"{num} is positive")
   elif num<0:
     print(f"{num} is negative")
 
